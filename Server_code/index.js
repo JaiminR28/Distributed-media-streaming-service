26365635704db7a0d72f52287ed3140c2ee8819a
Jaimin Rathwa <104933129+JaiminR28@users.noreply.github.com>
2023-05-15 13:02:32 +0530

    index.js and index.html file added

---
+++ Server_code/index.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const express = require("express");
+const video = require("fluent-ffmpeg/lib/options/video");
+const fs = require("fs");
+const app = express();
+
+app.get("/", function (req, res) {
+	res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/video", function (req, res) {
+	const range = req.headers.range;
+	if (!range) {
+		res.status(400).send("Required Range header");
+	}
+
+	const videoPath = "demo.mp4";
+	const videoSize = fs.statSync("demo.mp4").size;
+
+	const CHUNK_SIZE = 10 ** 6;
+	const start = Number(range.replace(/\D/g, ""));
+	const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+	const contentLength = end - start + 1;
+
+	const headers = {
+		"Content-Range": `bytes ${start}-${end}/${videoSize}`,
+		"Accept-Ranges": "bytes",
+		"Content-Length": contentLength,
+		"Content-Type": "video/mp4",
+	};
+
+	res.writeHead(206, headers);
+	console.log(headers);
+	const videoStream = fs.createReadStream(videoPath, { start, end });
+
+	videoStream.pipe(res);
+});
+
+app.listen(8001, function () {
+	console.log("Listening to the port 8001");
+});
